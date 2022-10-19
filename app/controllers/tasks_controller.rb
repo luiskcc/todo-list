ccfc54ba4f80937315e8076db406e64a104e6580
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2022-10-19 23:46:41 +0100

    CRUD added to controllers and views based on routes paths

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,55 @@
 class TasksController < ApplicationController
+
+    before_action :find_taks, only:[:show, :edit, :update, :destroy ]
+
+
+
     def index
         @tasks = Task.all.order( "created_at DESC")
 
     end
 
+
+
+    def show
+
+        @task = Task.find(find_task) 
+
+    end
+
+
+
     def new
         @task  = Task.new
+
+
+    end
+
+
+    def edit
+
+         @task = Task.find(find_task) 
+
+    end
+
+
+     
+    def update
+
+        if @task.update(task_params)
+            redirect_to task_path(@task)
+
+        else
+            render 'edit'
+        end
+
+    end
+
+    def destroy
+
+        @item.destroy
+        redirect_to root_path
+
 
     end
 
@@ -22,5 +66,9 @@ class TasksController < ApplicationController
     private
     def task_params 
         params.require(:task).permit(:title, :description, :status)
+    end
+
+    def find_task
+         @task = Task.fin(params[:id])
     end
 end
