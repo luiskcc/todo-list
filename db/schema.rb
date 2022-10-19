8e6e822938e0d66b158a8992ec907d3d64110b84
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2022-10-19 20:39:37 +0100

    tasks model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_19_185418) do
+ActiveRecord::Schema.define(version: 2022_10_19_190515) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
