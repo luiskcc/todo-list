987fd0b70b4dd6cc933a2410a0aa4b361e2e4813
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2022-10-19 21:16:31 +0100

    tasks controller def (new, create, params)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,24 @@
 class TasksController < ApplicationController
     def index
-        
+    end
+
+    def new
+        @task  = Task.new
+
+    end
+
+    def create
+        @task= Task.new(task_params)
+        if @task.save
+            redirect_to root_path
+
+        else render 'new'
+        end
+
+    end
+
+    private
+    def task_params 
+        params.require(:task).permit(:title, :description, :status)
     end
 end
